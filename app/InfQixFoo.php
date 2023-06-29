afcc957fdb8d1983ac428c1432f7619b966735a9
azemitis <7-days@inbox.lv>
2023-06-29 19:33:03 +0300

    Implementation of Step 4

---
+++ app/InfQixFoo.php
@@ -0,0 +1,85 @@
+<?php declare(strict_types=1);
+
+namespace App;
+
+use InvalidArgumentException;
+
+class InfQixFoo
+{
+    private array $rules;
+
+    public function __construct()
+    {
+        $this->rules = [
+            8 => 'Inf',
+            7 => 'Qix',
+            3 => 'Foo',
+        ];
+    }
+
+    public function combined(int $number): string
+    {
+        $multipleResult = $this->multiple($number);
+        $occurrenceResult = $this->occurrence($number);
+
+        $result = $multipleResult;
+        if ($multipleResult !== '' && $occurrenceResult !== '') {
+            $result .= '; ';
+        }
+        $result .= $occurrenceResult;
+
+        if ($result !== '') {
+            return $result;
+        } else {
+            return (string) $number;
+        }
+    }
+
+    public function multiple(int $number): string
+    {
+        if ($number <= 0) {
+            throw new InvalidArgumentException('Number must be a positive integer.');
+        }
+
+        $result = '';
+
+        foreach ($this->rules as $divisor => $output) {
+            if ($number % $divisor === 0) {
+                $result .= $output . '; ';
+            }
+        }
+
+        $trimmedResult = rtrim($result, '; ');
+
+        if ($trimmedResult !== '') {
+            return $trimmedResult;
+        } else {
+            return (string) $number;
+        }
+    }
+
+    public function occurrence(int $number): string
+    {
+        if ($number <= 0) {
+            throw new InvalidArgumentException('Number must be a positive integer.');
+        }
+
+        $result = [];
+
+        $digits = str_split((string) $number);
+
+        foreach ($digits as $digit) {
+            if (isset($this->rules[$digit])) {
+                $result[] = $this->rules[$digit];
+            }
+        }
+
+        $result = implode('; ', $result);
+
+        if ($result !== '') {
+            return $result;
+        } else {
+            return '';
+        }
+    }
+}
