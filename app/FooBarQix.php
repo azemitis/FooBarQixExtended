cf1488b069da87ff4b375754012516988426315c
azemitis <7-days@inbox.lv>
2023-06-29 15:31:35 +0300

    Implementation of Step 3

--- app/FooBarQix.php
+++ app/FooBarQix.php
@@ -17,6 +17,24 @@ class FooBarQix
         ];
     }
 
+    public function combined(int $number): string
+    {
+        $multipleResult = $this->multiple($number);
+        $occurrenceResult = $this->occurrence($number);
+
+        $result = $multipleResult;
+        if ($multipleResult !== '' && $occurrenceResult !== '') {
+            $result .= ', ';
+        }
+        $result .= $occurrenceResult;
+
+        if ($result !== '') {
+            return $result;
+        } else {
+            return (string) $number;
+        }
+    }
+
     public function multiple(int $number): string
     {
         if ($number <= 0) {
@@ -35,6 +53,31 @@ class FooBarQix
 
         if ($trimmedResult !== '') {
             return $trimmedResult;
+        } else {
+            return (string) $number;
+        }
+    }
+
+    public function occurrence(int $number): string
+    {
+        if ($number <= 0) {
+            throw new InvalidArgumentException('Number must be a positive integer.');
+        }
+
+        $result = [];
+
+        $digits = str_split((string) $number);
+
+        foreach ($digits as $digit) {
+            if (isset($this->rules[$digit])) {
+                $result[] = $this->rules[$digit];
+            }
+        }
+
+        $result = implode(', ', $result);
+
+        if ($result !== '') {
+            return $result;
         } else {
             return (string) $number;
         }
